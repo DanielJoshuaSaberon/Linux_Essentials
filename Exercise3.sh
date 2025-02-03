5f8179301c9180c340326f043be36edd33b04ca5
Daniel Joshua P. Saberon <152839529+DanielJoshuaSaberon@users.noreply.github.com>
2025-02-03 07:18:28 +0000

    initial commit

--- Exercise3.sh
+++ Exercise3.sh
@@ -5,7 +5,7 @@ x=8
 y=2
 
 
-echo $x + $y = echo $(expr $x + $y) 
+echo $x + $y = $(expr $x + $y) 
 echo $x - $y = $(expr $x - $y) 
 echo $x x $y = $(expr $x \* $y) 
 echo $x / $y = $(expr $x / $y) 
@@ -16,4 +16,3 @@ echo $x - 5 = $(expr $x - 5)
 echo $x x 5 = $(expr $x \* 5)
 echo $x / 5 = $(expr $x / 5)
 echo $x % 5 = $(expr $x % 5)
-
