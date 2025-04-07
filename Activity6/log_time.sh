b9730cdb381dd6f24ec4cd34160737c5cdb11d63
Daniel Joshua P. Saberon <152839529+DanielJoshuaSaberon@users.noreply.github.com>
2025-04-07 05:44:29 +0000

    updated

--- Activity6/log_time.sh
+++ Activity6/log_time.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo "Current Date and Time: $(date)" >> Activity6/log.txt 
+
+echo "Log updated by $(whoami) at $(date)" >> Activity6/log.txt
+
+log_count=$(wc -l < Activity6/log.txt)
+echo "The log file has been updated $log_count times." >> Activity6/log.txt
